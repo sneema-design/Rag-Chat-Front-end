97825b42fb90beaf5ad2b773e04bffbe32af33c5
sneema-design <sneema@gammastack.com>
2026-03-12 14:46:55 +0530

    feat-added login api

--- app/services/auth/useAuthService.ts
+++ app/services/auth/useAuthService.ts
@@ -1,10 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
-import { signUp } from "./authService";
+import { login, signUp } from "./authService";
 import type { signUpResponse, signUpValues } from "@/app/types/signUpType";
+import { loginResponse, loginValue } from "@/app/types/loginType";
 
 
 export const useSignup=()=>{
     return useMutation<signUpResponse,Error,signUpValues>({
         mutationFn:(singUpData:signUpValues)=> signUp(singUpData)
+    })
+}
+
+export const useLogin=()=>{
+    return useMutation<loginResponse,Error,loginValue>({
+        mutationFn:(loginData:loginValue)=>login(loginData)
     })
 }
