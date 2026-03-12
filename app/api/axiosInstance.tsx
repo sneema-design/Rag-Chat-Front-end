2691824970253e30364fa40695eb01c91c7ddeeb
sneema-design <sneema@gammastack.com>
2026-03-12 14:22:43 +0530

    feat-added signup api

--- app/api/axiosInstance.tsx
+++ app/api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api=axios.create({
-    baseURL:"http://localhost:4000/ask",
+    baseURL:"http://localhost:4000",
     headers:{
         "Content-Type":"application/json"
     }
